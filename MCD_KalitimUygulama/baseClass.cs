2952a0655050fb10f6f44cd6b1ef1575693e456b
emre gemici <gmcc.emree@gmail.com>
2021-12-02 19:50:30 +0300

    Add project files.

---
+++ MCD_KalitimUygulama/baseClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_KalitimUygulama
+{
+    public class baseClass
+    {
+        public static int sayac = 1; // ilk oluştuğu zaman 1 değerini otomatik olarak alsın.
+
+        private int _id;
+        public int id 
+        {
+            get
+            {
+                return _id;
+            }
+            private set
+            {
+                //Bir sayaç içerisindeki değerden faydalanmak istiyoruz.
+                //_id = sayac;
+                //sayac = sayac+1;
+                //Nedeni basit set alanı private olarak işaretlendi.
+            }
+        }
+
+        public baseClass()
+        {
+            _id = sayac;
+            sayac = sayac + 1;
+        }
+
+        private string _barkod;
+        public string barkod { get; set; } //TODO: Buraya geri dön.
+        public DateTime olusturmaTarih { get; set; }
+        public int  olusturanKullanici { get; set; }
+        public DateTime  guncellemeTarih { get; set; }
+        public int guncelleyenKullanici { get; set; }
+        public bool silindi { get; set; }
+
+    }
+}
